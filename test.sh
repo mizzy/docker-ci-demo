f3bcc9b534e284e70c54fd7d23fa916d0eb6cad6
Gosuke Miyashita <gosukenator@gmail.com>
2013-07-19 16:48:59 +0000

    Update

--- test.sh
+++ test.sh
@@ -3,7 +3,11 @@
 cd /docker-ci-demo
 # git pull origin master
 
+echo "********** Running puppet apply ... **********"
+echo
 puppet apply httpd.pp --color=false
 
+echo "********** Running serverspec ... **********"
+echo 
 rake spec
 
